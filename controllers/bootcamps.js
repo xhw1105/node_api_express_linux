8b64b1ac8f6b506da8ebd82f031c502f4aa07843
Sasha <xhw1105@gmail.com>
2020-09-03 20:32:55 +1200

    added post method

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,16 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
 exports.getBootcamps = (req, res, next) => {
   res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+};
+
+// @desc    Create new bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({ success: true, msg: 'Create new bootcamp' });
 };
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const { getBootcamps } = require('../controllers/bootcamps');
+const { getBootcamps, createBootcamp } = require('../controllers/bootcamps');
 
 const router = express.Router();
 
-router.route('/').get(getBootcamps);
+router.route('/').get(getBootcamps).post(createBootcamp);
 
 // router.get('/:id', (req, res) => {
 //   res
